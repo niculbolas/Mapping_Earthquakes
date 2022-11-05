17b68091b5e7843533388bebfcc15fd4016f56ac
niculbolas <105987416+niculbolas@users.noreply.github.com>
2022-11-05 13:55:22 -0400

    Added json file for airport data.

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,22 +1,15 @@
-// Add console.log to check to see if our code is working.
-
-console.log("working");
-
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([30, 30], 2);
 
-// Coordinates for each point to be used in the polyline
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
+// Add GeoJSON data.
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup;
+    }}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
